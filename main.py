093341fb966af9edb916b2e58aae6be85e002f24
User <kd200681@mail.ru>
2023-10-15 23:59:53 +0300

    No decorator

--- main.py
+++ main.py
@@ -43,6 +43,31 @@ async def send_echo(message: Message):
     # await bot.send_message(message.chat.id, message.text)
 
 
+# Без декораторов
+# Этот хэндлер будет срабатывать на команду "/start"
+# async def process_start_command(message: Message):
+#     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+#
+#
+# # Этот хэндлер будет срабатывать на команду "/help"
+# async def process_help_command(message: Message):
+#     await message.answer(
+#         'Напиши мне что-нибудь и в ответ '
+#         'я пришлю тебе твое сообщение'
+#     )
+#
+#
+# # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# # кроме команд "/start" и "/help"
+# async def send_echo(message: Message):
+#     await message.reply(text=message.text)
+#
+#
+# # Регистрируем хэндлеры
+# dp.message.register(process_start_command, Command(commands='start'))
+# dp.message.register(process_help_command, Command(commands='help'))
+# dp.message.register(send_echo)
+
 # Она запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота.
 # В качестве аргумента в метод диспетчера run_polling нужно передать объект бота
 if __name__ == '__main__':
