6acd446c142c3c6bfae7e052224e500822b8c883
User <kd200681@mail.ru>
2023-10-15 23:51:04 +0300

    Eho

---
+++ main.py
@@ -0,0 +1,49 @@
+from aiogram import Bot, Dispatcher
+# Из aiogram.filters импортируем класс Command, чтобы фильтровать апдейты по наличию в них команд,
+# то есть сочетаний символов, начинающихся со знака "/"
+from aiogram.filters import Command
+# Из  aiogram.types импортируем класс Message. Апдейты этого типа мы будем ловить эхо-ботом
+from aiogram.types import Message
+
+from data import config
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+#  По сути - это задекорированная асинхронная функция, которая на вход принимает объект типа Message, если срабатывает
+#  фильтр, указанный в декораторе (в данном случае через фильтр Command(commands=["start"])
+#  как раз и пройдет только команда /start)
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    # через метод answer объекта типа Message хэндлер отправит в чат пользователю приветственное сообщение
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+    # чтобы он отправил его в канал или группу, или отдельным контактам, то есть не в тот же самый чат,
+    # куда вы прислали сообщение боту)
+    # await bot.send_message(chat_id='ID или название чата', text='Какой-то текст')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+    # Строго говоря, и в тот же самый чат можно ответить этим способом, вот так:
+    # await bot.send_message(message.chat.id, message.text)
+
+
+# Она запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота.
+# В качестве аргумента в метод диспетчера run_polling нужно передать объект бота
+if __name__ == '__main__':
+    dp.run_polling(bot)
